f022687636032c167816a3ff09f3ac47c7ff34ca
LTKSK <komari0714@gmail.com>
2018-07-25 23:32:48 +0900

    [add] 簡易的な文字列追加部分の実装

---
+++ src/components/Chat/Chat.vue
@@ -0,0 +1,32 @@
+<template>
+<div>Hello Chat at {{ channel }}
+  <div>
+    <ul>
+      <li v-for="msg in messages">{{ msg }}</li>
+    </ul>
+  </div>
+  <div>
+    <input type="text" v-model="message">
+    <input type="button" value="send" v-on:click="send_message" />
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'chat',
+  data() {
+    return {
+      channel: this.$route.params.channelname,
+      message: "",
+      messages: [],
+    }
+  },
+  methods: {
+    send_message() {
+      this.messages.push(this.message);
+      this.message = "";
+    }
+  }
+}
+</script>
