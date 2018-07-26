01f04ca497a8376911d93b0a00473ec5edc309e8
LTKSK <ltksk0714@gmail.com>
2018-07-26 23:36:50 +0900

    [modify] styleを調整

--- src/components/Chat/Chat.vue
+++ src/components/Chat/Chat.vue
@@ -1,11 +1,23 @@
 <template>
-<div>Hello Chat at {{ channel }}
-  <div>
+<div>
+  <div class="channel-name">
+    Hello Chat at {{ channel }}
+  </div>
+  <div class="view">
     <ul>
-      <li v-for="msg in messages">{{ msg }}</li>
+      <li v-for="channel in channels">
+        <router-link v-bind:to="{name: 'channel', params: {channelname: channel}}">
+          {{ channel }}
+        </router-link>
+      </li>
     </ul>
   </div>
   <div>
+    <ul class="message-list">
+      <li v-for="msg in messages">{{ msg }}</li>
+    </ul>
+  </div>
+  <div class="message-form">
     <input type="text" v-model="message">
     <input type="button" value="send" v-on:click="send_message" />
   </div>
@@ -18,6 +30,7 @@ export default {
   data() {
     return {
       channel: this.$route.params.channelname,
+      channels: ["movie", "image"],
       message: "",
       messages: [],
     }
@@ -30,3 +43,21 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+}
+.channel-name {
+  text-align: center;
+}
+.message-list {
+  text-align: center;
+  grid-area: 2/2;
+  height: 300px;
+  overflow: scroll;
+}
+.message-form {
+  text-align: center;
+}
+</style>
