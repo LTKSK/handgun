d6395514dcae6757b945d16dd9b448bdbd9dc030
LTKSK <ltksk0714@gmail.com>
2018-07-31 00:41:19 +0900

    [add] shaderのloadまでを実装

--- src/components/ReviewTarget/Image/image.js
+++ src/components/ReviewTarget/Image/image.js
@@ -1,10 +1,41 @@
 export default {
   name: "image-item",
   data() {
+    const initShaders = (gl) => {
+      const vertexShaderSource = document.querySelector("#vs").text
+      const fragmentShaderSource = document.querySelector("#fs").text
+      // create shader program
+      const vertexShader= gl.createShader(gl.VERTEX_SHADER)
+      gl.shaderSource(vertexShader, vertexShaderSource)
+      gl.compileShader(vertexShader)
+      // check compile status
+      const vShaderComplieStatus = gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)
+      if(!vShaderComplieStatus) {
+        const info = gl.getShaderInfoLog(vertexShader)
+        console.log(info)
+      }
+
+      const fragmentShader= gl.createShader(gl.FRAGMENT_SHADER)
+      gl.shaderSource(fragmentShader, fragmentShaderSource)
+      gl.compileShader(fragmentShader)
+      // check compile status
+      const fShaderComplieStatus = gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)
+      if(!fShaderComplieStatus) {
+        const info = gl.getShaderInfoLog(fragmentShader)
+        console.log(info)
+      }
+      const shaderProgram = gl.createProgram()
+      gl.attachShader(shaderProgram, vertexShader)
+      gl.attachShader(shaderProgram, fragmentShader)
+      gl.linkProgram(shaderProgram)
+      if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        console.log("Shader initialize failed")
+      }
+    }
     const initWebGL = canvas => {
       let gl = null
       try {
-        gl = canvas.getContext("webgl")
+        gl = canvas.getContext("webgl2")
       }
       catch (e) {
         console.log(e)
@@ -16,6 +47,8 @@ export default {
     }
     const start = () => {
       const canvas = document.querySelector("#glcanvas")
+      canvas.width = 640
+      canvas.height = 480
       const gl = initWebGL(canvas)
       if (gl) {
         gl.clearColor(0.0, 0.0, 0.0, 1.0)
@@ -23,6 +56,7 @@ export default {
         gl.depthFunc(gl.LEQUAL)
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
       }
+      initShaders(gl)
     }
     return {
       name: "tmp",
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+// import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/ja'
 
 import App from './App'
@@ -10,7 +10,7 @@ import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
-Vue.use(ElementUI, { locale })
+// Vue.use(ElementUI, { locale })
 
 /* eslint-disable no-new */
 new Vue({
