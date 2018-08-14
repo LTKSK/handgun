e327f0ed5f46aa4fe02fc5985dd6fd1124f4394b
LTKSK <ltksk0714@gmail.com>
2018-08-14 23:47:11 +0900

    [modify] 複数の線を引けるように変更

--- src/components/ReviewTarget/Image/image.js
+++ src/components/ReviewTarget/Image/image.js
@@ -102,38 +102,6 @@ export default {
       gl.flush()
     },
 
-    drawLines(gl) {
-      const program = this.createShaderProgram(gl, "#line-vs", "#line-fs")
-      //prepare buffers
-      const vertexBuffer = gl.createBuffer()
-      const colorBuffer = gl.createBuffer()
-      const vertexAttribLocation = gl.getAttribLocation(program, "vertexPosition")
-      const colorAttribLocation = gl.getAttribLocation(program, "color")
-      const VERTEX_SIZE = 3
-      const COLOR_SIZE = 4
-      // should bind before binding buffer
-      gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
-      // enable 'in' variables
-      gl.enableVertexAttribArray(vertexAttribLocation)
-      gl.vertexAttribPointer(vertexAttribLocation, VERTEX_SIZE, gl.FLOAT, false, 0, 0)
-
-      gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer)
-      gl.enableVertexAttribArray(colorAttribLocation)
-      gl.vertexAttribPointer(colorAttribLocation, COLOR_SIZE, gl.FLOAT, false, 0, 0)
-      const vertices = new Float32Array(this.vertexs)
-      const colors = new Float32Array(this.colors)
-
-      gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
-      gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
-      gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer)
-      gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW)
-
-      // draw
-      const vertex_size = vertices.length / 3
-      gl.drawArrays(gl.LINE_STRIP, 0, vertex_size)
-      gl.flush()
-    },
-
     initWebGL() {
       const canvas = document.querySelector("#glcanvas")
       canvas.width = 640
@@ -161,31 +129,72 @@ export default {
       this.drawImage(gl)
     },
 
+    drawLines(gl) {
+      const program = this.createShaderProgram(gl, "#line-vs", "#line-fs")
+      //prepare buffers
+      const vertexBuffer = gl.createBuffer()
+      const colorBuffer = gl.createBuffer()
+      const vertexAttribLocation = gl.getAttribLocation(program, "vertexPosition")
+      const colorAttribLocation = gl.getAttribLocation(program, "color")
+      const VERTEX_SIZE = 3
+      const COLOR_SIZE = 4
+      // should bind before binding buffer
+      gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
+      // enable 'in' variables
+      gl.enableVertexAttribArray(vertexAttribLocation)
+      gl.vertexAttribPointer(vertexAttribLocation, VERTEX_SIZE, gl.FLOAT, false, 0, 0)
+
+      gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer)
+      gl.enableVertexAttribArray(colorAttribLocation)
+      gl.vertexAttribPointer(colorAttribLocation, COLOR_SIZE, gl.FLOAT, false, 0, 0)
+
+      const vertices = new Float32Array(this.vertices)
+      const vertex_count = vertices.length / 3
+      const color_list = []
+      for (let i=0; i<vertex_count; ++i) {
+        Array.prototype.push.apply(color_list, this.color)
+      }
+      const colors = new Float32Array(color_list)
+
+      gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
+      gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.DYNAMIC_DRAW)
+      gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer)
+      gl.bufferData(gl.ARRAY_BUFFER, colors, gl.DYNAMIC_DRAW)
+      // draw
+      for (let index=0; index<this.start_indices.length; ++index){
+        const first = this.start_indices[index]
+        const last = this.vertex_counts[index]
+        gl.drawArrays(gl.LINE_STRIP, first, last)
+      }
+      gl.flush()
+    },
+
     eventSetup() {
       const canvas = document.querySelector("#glcanvas")
       // setup mouse callbacks
       canvas.addEventListener("mouseup", () => {
         this.on_click = false
+        ++this.current_index
       })
       canvas.addEventListener("mousedown", () => {
         this.on_click = true
+        this.vertex_counts.push(0)
+        this.start_indices.push(this.vertex_count)
       })
       canvas.addEventListener("mousemove", (event) => {
         if(! this.on_click) {
           return
         }
+        ++this.vertex_count
+        ++this.vertex_counts[this.current_index]
         const rect = event.target.getBoundingClientRect()
         const current_x = event.clientX - rect.left
         const current_y = event.clientY - rect.top
-
         // current potisions normalized by canvas -0.5~0.5
-        this.vertexs.push((current_x / canvas.width)*2.0-1.0)
+        this.vertices.push((current_x / canvas.width)*2.0-1.0)
         // to reverse y position because texture origin position is started from left upper
-        this.vertexs.push((1.0 - current_y / canvas.height)*2.0-1.0)
-        this.vertexs.push(0.0)
-        for (let i=0; i<4; ++i){
-          this.colors.push(1.0)
-        }
+        this.vertices.push((1.0 - current_y / canvas.height)*2.0-1.0)
+        this.vertices.push(0.0)
         this.drawImage(this.gl)
         this.drawLines(this.gl)
       })
@@ -196,8 +205,12 @@ export default {
       gl: null,
       name: "review",
       on_click: false,
-      vertexs: [],
-      colors: [],
+      vertices: [],
+      vertex_count: 0,
+      current_index: 0,
+      start_indices: [],
+      vertex_counts: [],
+      color: [1.0, 1.0, 1.0, 1.0],
     }
   },
   mounted() {
