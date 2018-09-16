aac5bd4b0bb3a08575f26dc1d6c8c938f71592a0
LTKSK <ltksk0714@gmail.com>
2018-09-16 18:08:04 +0900

    [modity] message一覧の表示を調整。message一覧のソートが正しく行えていなかった問題を修正

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,2 +1,7 @@
-export const messages = state => state.messages.reverse()
+// export const messages = state => state.messages.reverse()
+export const messages = state => state.messages.sort((a, b) => {
+  if(a.index > b.index) return -1
+  if(a.index < b.index) return 1
+  return 0
+})
 export const channels = state => state.channels
