812499c90b64577141a5ce61e2ffc76443226a68
LTKSK <ltksk0714@gmail.com>
2018-09-22 12:00:43 +0900

    [modify] usernameとpasswordにvalidate処理を追加

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -4,18 +4,60 @@ export default {
   name: "register-form",
   data() {
     return {
-      username: "",
-      password: "",
-      disabled: false,
+      form: {
+        username: "",
+        password: "",
+      },
+      rules: {
+        username: [
+          {
+            required: true,
+            message: "username is required",
+            trigger: 'blur'
+          },
+          {
+            message: "Please dont input space only",
+            whitespace: true,
+            trigger: 'blur'
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: "password is required",
+            trigger: 'blur'
+          },
+          {
+            min: 8,
+            max: 24,
+            message: "password must be between 8 to 24 characters",
+            trigger: 'blur'
+          }
+        ],
+      }
     }
   },
   methods: {
     ...mapActions([
       REGISTER_USER,
     ]),
-    register() {
-      this.REGISTER_USER({"username": this.username,
-                          "password": this.password})
+    registration_succeeded() {
+      this.$notify({
+        title: "Success!",
+        message: "Registration succeeded",
+        type: 'success'
+      });
+    },
+    register(form_name) {
+      this.$refs[form_name].validate((is_valid) => {
+        if (is_valid) {
+          this.REGISTER_USER({"username": this.form.username,
+                              "password": this.form.password})
+          this.registration_succeeded()
+        } else {
+          return false;
+        }
+      });
     }
   }
 }
