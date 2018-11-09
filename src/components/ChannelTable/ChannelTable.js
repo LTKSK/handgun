6d2f139d9588a1c98eea404061bea089dfec515c
LTKSK <ltksk0714@gmail.com>
2018-11-10 00:04:38 +0900

    [add] Channelのimaege一覧を表示できるComponent用のファイルを追加

---
+++ src/components/ChannelTable/ChannelTable.js
@@ -0,0 +1,6 @@
+export default {
+  name: 'channel-table',
+  data() {
+    return {}
+  }
+}
---
+++ src/components/ChannelTable/index.vue
@@ -0,0 +1,3 @@
+<template src="./ChannelTable.html" />
+<script src="./ChannelTable.js" />
+<style src="./ChannelTable.css" scoped />
