90bc5672efb62c994089f122df1eeed27f548fd7
LTKSK <ltksk0714@gmail.com>
2018-11-11 18:22:48 +0900

    [add] channel一覧表示機能を実装中

--- src/components/ChannelTable/ChannelTable.js
+++ src/components/ChannelTable/ChannelTable.js
@@ -1,6 +1,30 @@
+import { mapGetters, mapActions } from 'vuex'
+import { GET_CHANNELS } from '@/store/mutation-types'
+
 export default {
   name: 'channel-table',
   data() {
     return {}
+  },
+  computed: {
+    ...mapGetters([
+      'channels',
+      'header'
+    ]),
+  },
+  methods: {
+    ...mapActions([
+      GET_CHANNELS,
+    ]),
+  },
+  mounted() {
+    this.GET_CHANNELS(this.header)
+      .catch(error => {
+        this.$notify({
+          title: "Failed!",
+          message: `get channel list failed. ${error.message}`,
+          type: "error"
+        })
+    })
   }
 }
