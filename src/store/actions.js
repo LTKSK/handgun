fb273a47bc9c4d542212cbeecb928ebbaa60245f
LTKSK <ltksk0714@gmail.com>
2018-12-21 00:05:14 +0900

    [modify] mutation-typeのimportの仕方を整理

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,19 +1,4 @@
-import {
-  LOGIN,
-  LOGOUT,
-  GET_MESSAGES,
-  EDIT_MESSAGE,
-  ADD_MESSAGE,
-  DELETE_MESSAGE,
-  GET_CHANNELS,
-  ADD_CHANNEL,
-  DELETE_CHANNEL,
-  GET_ICON,
-  GET_USERS,
-  GET_LAYERS,
-  UPDATE_LAYERS,
-  DELETE_LAYER
-} from './mutation-types'
+import * as types from './mutation-types'
 import {
   login,
   fetchMessages,
@@ -33,19 +18,19 @@ import {
 import moment from 'moment'
 
 export default {
-  async [LOGIN]({ commit }, payload) {
+  async [types.LOGIN]({ commit }, payload) {
     const response = await login(payload.username, payload.password)
     const json = await response.json()
-    commit(LOGIN, {jwt: json.token, user: payload.username})
+    commit(types.LOGIN, {jwt: json.token, user: payload.username})
   },
-  async [LOGOUT]({ commit }) {
-    commit(LOGOUT)
+  async [types.LOGOUT]({ commit }) {
+    commit(types.LOGOUT)
   },
-  async [GET_MESSAGES]({ commit }, channel_name) {
+  async [types.GET_MESSAGES]({ commit }, channel_name) {
     const messages = await fetchMessages(channel_name)
-    commit(GET_MESSAGES, messages)
+    commit(types.GET_MESSAGES, messages)
   },
-  async [ADD_MESSAGE]({ commit }, payload) {
+  async [types.ADD_MESSAGE]({ commit }, payload) {
     const message_data = {
       "index": payload.index,
       "value": payload.message,
@@ -54,52 +39,52 @@ export default {
       "layer_id": payload.layer_id
     }
     await postMessage(payload.channel_name, message_data)
-    commit(ADD_MESSAGE, message_data)
+    commit(types.ADD_MESSAGE, message_data)
   },
-  async [EDIT_MESSAGE]({ commit }, message) {
+  async [types.EDIT_MESSAGE]({ commit }, message) {
     await putMessage(message)
-    commit(EDIT_MESSAGE, message)
+    commit(types.EDIT_MESSAGE, message)
   },
-  async [DELETE_MESSAGE]({ commit }, message) {
+  async [types.DELETE_MESSAGE]({ commit }, message) {
     await deleteMessage(message.channel, message.index)
-    commit(DELETE_MESSAGE, message)
+    commit(types.DELETE_MESSAGE, message)
   },
-  async [GET_CHANNELS]({ commit, state }) {
+  async [types.GET_CHANNELS]({ commit, state }) {
     const header = {"Authorization": `Bearer ${state.jwt}`}
     const channels = await fetchChannels(header)
-    commit(GET_CHANNELS, channels)
+    commit(types.GET_CHANNELS, channels)
   },
-  async [ADD_CHANNEL]({ commit, state }, payload) {
+  async [types.ADD_CHANNEL]({ commit, state }, payload) {
     const header = {"Authorization": `Bearer ${state.jwt}`}
     await postChannel(payload.channel, header)
     await postReviewTarget(payload.channel, payload.file)
-    commit(ADD_CHANNEL, payload.channel)
+    commit(types.ADD_CHANNEL, payload.channel)
   },
-  async [DELETE_CHANNEL]({ commit, state }, channel) {
+  async [types.DELETE_CHANNEL]({ commit, state }, channel) {
     const header = {"Authorization": `Bearer ${state.jwt}`}
     await deleteChannel(channel, header)
-    commit(DELETE_CHANNEL, channel)
+    commit(types.DELETE_CHANNEL, channel)
   },
-  async [GET_ICON]({ commit }, username) {
+  async [types.GET_ICON]({ commit }, username) {
     const icon = await fetchUserIcon(username)
-    commit(GET_ICON, {username, icon})
+    commit(types.GET_ICON, {username, icon})
   },
-  async [GET_USERS]({ commit }) {
+  async [types.GET_USERS]({ commit }) {
     const users = await fetchUsers()
-    commit(GET_USERS, users)
+    commit(types.GET_USERS, users)
   },
-  async [GET_LAYERS]({ commit }, channel_name) {
+  async [types.GET_LAYERS]({ commit }, channel_name) {
     const layers = await fetchLayers(channel_name)
-    commit(GET_LAYERS, layers)
+    commit(types.GET_LAYERS, layers)
   },
-  async [UPDATE_LAYERS]({ commit }, payload) {
+  async [types.UPDATE_LAYERS]({ commit }, payload) {
     await putLayers(payload.channel_name, payload.layers)
-    commit(UPDATE_LAYERS, payload.layers)
+    commit(types.UPDATE_LAYERS, payload.layers)
   },
-  async [DELETE_LAYER]({ commit }, payload) {
+  async [types.DELETE_LAYER]({ commit }, payload) {
     await deleteLayer(payload.channel_name, payload.layer)
-    commit(DELETE_LAYER, payload.layer)
+    commit(types.DELETE_LAYER, payload.layer)
     const messages = await fetchMessages(payload.channel_name)
-    commit(GET_MESSAGES, messages)
-  },
+    commit(types.GET_MESSAGES, messages)
+  }
 }
